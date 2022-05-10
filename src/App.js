7e7313378effdc1fe901f0958cefeb8d25e73b7c
Im-Oni <drackoshaaa@gmail.com>
2022-05-10 17:39:48 +0300

    add axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react/button-has-type */
+import React from 'react';
+import './styles/App.css';
+import axios from 'axios';
+import Header from './components/Header';
+// import UserList from './components/Userlist';
+
+async function fetchUsers() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  // eslint-disable-next-line no-console
+  console.log(response.data);
+}
+fetchUsers();
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <button onClick={fetchUsers}>get users</button>
     </div>
   );
 }
